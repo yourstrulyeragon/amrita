371d0099bf275ad43df75df21ff3620fff1b53bb
yourstrulyeragon <66470286+yourstrulyeragon@users.noreply.github.com>
2020-06-05 16:45:13 +0530

    Update saphira.sh

--- saphira.sh
+++ saphira.sh
@@ -8,9 +8,9 @@ sh ~amrita/update_saphira.sh;
 
 clear;
 
-tput bold setaf 1; echo -e "\n This script is to be used for a Torshammer DDoS attack. Have fun - say no to quantum torpedos and exams - Captain James Tiberius Kirk \n\n "; tput sgr0;
+tput setaf 1; echo -e "\n This script is to be used for a Torshammer DDoS attack. Have fun - say no to quantum torpedos and exams - Captain James Tiberius Kirk \n\n "; tput sgr0;
 
-tput bold setaf 6; echo -e "After connecting to Tor network, swipe the termux screen left to right to reveal a menu. Click New Session and run this program again. To exit, click exit in the notification menu. You can also press Ctrl+c multiple times, but pls don't tire yourself \n\n "
+tput setaf 6; echo -e "After connecting to Tor network, swipe the termux screen left to right to reveal a menu. Click New Session and run this program again. To exit, click exit in the notification menu. You can also press Ctrl+c multiple times, but pls don't tire yourself \n\n "
 
 echo "If you're using it for the first time, you might have to setup a few things. Don't worry, you won't get a blue screen of death. Just press 3 \n";
 
@@ -22,7 +22,9 @@ echo "If you want to run Torshammer in Tor relay press 1 \n";
 
 echo "If you want to run Torshammer without Tor, press 0 \n";
 
-echo " \n 	Enter your choice \n";
+tput sgr0;
+
+tput setaf 2; echo " \n 	Enter your choice \n";
 
 #This line reads user choice
 read -p 'Choose 1/2/3/0 : ' var;
@@ -56,6 +58,8 @@ then
 	n--
 	python2  ~/torshammer/torshammer.py -t ${arr[n]} -r 5000
 fi
+
+tput sgr0;
 
 termux-wake-unlock
 
